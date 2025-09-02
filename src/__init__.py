642ccfcb9b7bdef9907f23fc2c1ebbd5d216e154
jplanes <joel@engrate.io>
2025-09-02 15:55:39 +0200

    add prometheus instrumentation.

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(
     title="Power Tariffs Plugin",
     description="Plugin to provide power tariffs data and functionality",
 )
+
+Instrumentator().instrument(app).expose(app)
